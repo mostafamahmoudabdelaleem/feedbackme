3c2b88dfd5612df4ffdff2475abe5e5972602f59
Mostafa Mahmoud Abd El-Aleem <mostafamahmoudabdelalim@hotmail.com>
2020-10-31 16:36:08 +0200

    init commit

--- src/js/App.js
+++ src/js/App.js
@@ -1,11 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "../css/app.css";
 
 function App() {
+  console.log(process.env)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>FeedbackMe App</p>
         <p>Coming Soon ...</p>
       </header>
